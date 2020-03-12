9dfee4f6fe5e23034bb79d1bf5234cfeb03140c5
UrielAndrango <urielandrango@gmail.com>
2020-03-12 17:40:27 +0100

    Login v0.4.1

--- app/src/main/java/com/example/katundu/ui/login/RegisterActivity.java
+++ app/src/main/java/com/example/katundu/ui/login/RegisterActivity.java
@@ -3,17 +3,40 @@ package com.example.katundu.ui.login;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.katundu.R;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    //de momento asi pero hay que hacerle una clase especial
+    //o poner el codigo necesario dentro de esta clase
+    private RegisterViewModel registerViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
         //getSupportActionBar().hide();
-        
+
+        //No funciona porque no entiendo como funciona el original, pero no peta :)
+        final EditText passwordEditText = findViewById(R.id.password1);
+
+
+
+        passwordEditText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_ACTION_DONE) {
+                    registerViewModel.isPasswordValid(passwordEditText.getText().toString());
+                }
+                return false;
+            }
+        });
     }
 }
 
