974e32c7efa5c6352a15d64b12c523804ddc104f
annallanza <annallanza@hotmail.com>
2020-03-13 20:22:54 +0100

    Create LlistesUsuari.java

---
+++ app/src/main/java/com/example/katundu/ui/logged/LlistesUsuari.java
@@ -0,0 +1,34 @@
+package com.example.katundu.ui.logged;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.katundu.R;
+
+
+public class LlistesUsuari extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_llistes_usuari);
+        //Escondemos la Action Bar porque usamos la ToolBar
+        getSupportActionBar().hide();
+
+        final Button PerfilButton = findViewById(R.id.nomusuari);
+
+        PerfilButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LlistesUsuari.this, Perfil.class);
+                startActivity(intent);
+                //finish();
+            }
+        });
+    }
+}
+
