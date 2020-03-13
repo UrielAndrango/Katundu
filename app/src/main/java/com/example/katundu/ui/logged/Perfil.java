0c19669729926da8ea40165cfcf2606da4b08517
annallanza <annallanza@hotmail.com>
2020-03-13 21:01:22 +0100

    Update Perfil.java

--- app/src/main/java/com/example/katundu/ui/logged/Perfil.java
+++ app/src/main/java/com/example/katundu/ui/logged/Perfil.java
@@ -1,4 +1,34 @@
 package com.example.katundu.ui.logged;
 
-class Perfil {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.katundu.R;
+
+class Perfil extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_llistes_usuari);
+        //Escondemos la Action Bar porque usamos la ToolBar
+        getSupportActionBar().hide();
+
+        final ImageView Atras = findViewById(R.id.imageViewAtras);
+
+        Atras.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Perfil.this, LlistesUsuari.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+    }
 }
