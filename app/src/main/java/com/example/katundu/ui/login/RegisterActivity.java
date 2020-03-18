7afe82452ccc2952aac4c938048d07d05b20a227
annallanza <annallanza@hotmail.com>
2020-03-18 10:40:20 +0100

    Update RegisterActivity.java

--- app/src/main/java/com/example/katundu/ui/login/RegisterActivity.java
+++ app/src/main/java/com/example/katundu/ui/login/RegisterActivity.java
@@ -4,10 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.KeyEvent;
+import android.view.View;
 import android.view.inputmethod.EditorInfo;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.katundu.R;
 
 public class RegisterActivity extends AppCompatActivity {
@@ -25,6 +33,36 @@ public class RegisterActivity extends AppCompatActivity {
         //No funciona porque no entiendo como funciona el original, pero no peta :)
         final EditText passwordEditText = findViewById(R.id.password1);
 
+
+        final Button registrat = findViewById(R.id.button);
+
+
+        registrat.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View v) {
+                 // Instantiate the RequestQueue.
+                 RequestQueue queue = Volley.newRequestQueue(RegisterActivity.this);
+                 String url = "https://us-central1-test-8ea8f.cloudfunctions.net/addMessage?text=notinccabell";
+
+                 // Request a string response from the provided URL.
+                 StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
+                         new Response.Listener<String>() {
+                             @Override
+                             public void onResponse(String response) {
+                                 // Display the first 500 characters of the response string.
+                                 registrat.setText("Response is: " + response.substring(0, 500));
+                             }
+                         }, new Response.ErrorListener() {
+                             @Override
+                             public void onErrorResponse(VolleyError error) {
+                                 registrat.setText("That didn't work!");
+                             }
+                 });
+
+                 // Add the request to the RequestQueue.
+                 queue.add(stringRequest);
+             }
+         });
 
 
         passwordEditText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
