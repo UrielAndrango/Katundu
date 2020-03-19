ff02e5c8b3fc3dfa5f8504f14fc1de59c09dd44a
UrielAndrango <urielandrango@gmail.com>
2020-03-19 13:46:51 +0100

    Login v0.5.4

--- app/src/main/java/com/example/katundu/ui/logged/EditarPerfil.java
+++ app/src/main/java/com/example/katundu/ui/logged/EditarPerfil.java
@@ -3,11 +3,13 @@ package com.example.katundu.ui.logged;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.katundu.R;
+import com.example.katundu.ui.login.LoginActivity;
 
 public class EditarPerfil extends AppCompatActivity {
 
@@ -19,6 +21,7 @@ public class EditarPerfil extends AppCompatActivity {
         getSupportActionBar().hide();
 
         final ImageView Atras = findViewById(R.id.imageViewAtras);
+        final Button Logout = findViewById(R.id.logout);
 
         Atras.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -29,6 +32,15 @@ public class EditarPerfil extends AppCompatActivity {
             }
         });
 
+        Logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //FALTA HACER LOGOUT DE VERDAD
+                Intent intent = new Intent(EditarPerfil.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
 
