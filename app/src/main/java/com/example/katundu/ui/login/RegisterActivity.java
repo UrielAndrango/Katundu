de9ed90eb4b2907ce42fc267af44636127dc6c00
annallanza <annallanza@hotmail.com>
2020-03-21 00:56:48 +0100

    Register

--- app/src/main/java/com/example/katundu/ui/login/RegisterActivity.java
+++ app/src/main/java/com/example/katundu/ui/login/RegisterActivity.java
@@ -1,6 +1,7 @@
 package com.example.katundu.ui.login;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -17,6 +18,8 @@ import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.example.katundu.R;
 import com.example.katundu.ui.logged.MenuPrincipal;
+
+import java.net.URL;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -38,44 +41,48 @@ public class RegisterActivity extends AppCompatActivity {
         registrat.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Instantiate the RequestQueue.
-                RequestQueue queue = Volley.newRequestQueue(RegisterActivity.this);
-                StringBuilder url_builder = new StringBuilder("https://us-central1-test-8ea8f.cloudfunctions.net/signup?un=test&pw=1234&n=testname&lat=12&lon=34");
 
+                if(passwordEditText.getText() == repeatpasswordEditText.getText()) {
+                    // Instantiate the RequestQueue.
+                    RequestQueue queue = Volley.newRequestQueue(RegisterActivity.this);
 
-                url_builder.append(usernameEditText.getText());
-                url_builder.append(nameEditText.getText());
-                url_builder.append(passwordEditText.getText());
-                url_builder.append(repeatpasswordEditText.getText());
-                url_builder.append(latitudeEditText.getText());
-                url_builder.append(longitudeEditText.getText());
+                    String url = "https://us-central1-test-8ea8f.cloudfunctions.net/signup?" +
+                            "un=" + usernameEditText.getText() + "&" +
+                            "pw=" + passwordEditText.getText() + "&" +
+                            "n=" + nameEditText.getText() + "&" +
+                            "lat=" + latitudeEditText.getText() + "&" +
+                            "lon=" + longitudeEditText.getText();
 
-                String url = url_builder.toString();
+                    // Request a string response from the provided URL.
+                    StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
+                            new Response.Listener<String>() {
+                                @Override
+                                public void onResponse(String response) {
+                                    String welcome = getString(R.string.welcome) + usernameEditText.getText().toString();
+                                    Toast toast = Toast.makeText(getApplicationContext(), welcome, Toast.LENGTH_SHORT);
+                                    toast.show();
 
-                // Request a string response from the provided URL.
-                StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
-                    new Response.Listener<String>() {
+                                    Intent intent = new Intent(RegisterActivity.this, MenuPrincipal.class);
+                                    startActivity(intent);
+                                    //finish();
+                                }
+                            }, new Response.ErrorListener() {
                         @Override
-                        public void onResponse(String response) {
-                            String welcome = getString(R.string.welcome) + usernameEditText.getText().toString();
-                            Toast toast = Toast.makeText(getApplicationContext(), welcome, Toast.LENGTH_SHORT);
+                        public void onErrorResponse(VolleyError error) {
+                            String texterror = getString(R.string.incorrect_data);
+                            Toast toast = Toast.makeText(RegisterActivity.this, texterror, Toast.LENGTH_SHORT);
                             toast.show();
-
-                            Intent intent = new Intent(RegisterActivity.this, MenuPrincipal.class);
-                            startActivity(intent);
-                            //finish();
                         }
-                    }, new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        String texterror = getString(R.string.incorrect_data);
-                        Toast toast = Toast.makeText(RegisterActivity.this, texterror, Toast.LENGTH_SHORT);
-                        toast.show();
-                    }
-                });
+                    });
 
-                // Add the request to the RequestQueue.
-                queue.add(stringRequest);
+                    // Add the request to the RequestQueue.
+                    queue.add(stringRequest);
+                }
+                else {
+                    String texterror = getString(R.string.incorrect_password);
+                    Toast toast = Toast.makeText(RegisterActivity.this, texterror, Toast.LENGTH_SHORT);
+                    toast.show();
+                }
             }
         });
     }
