f81807e5aa4b9d1f5e93a497daa50a8f15b8cd47
UrielAndrango <urielandrango@gmail.com>
2020-04-23 10:42:02 +0200

    Listas con Foto (Layout de dos layouts que contienen: foto+precio+nombre)

--- app/src/main/java/com/example/katundu/ui/logged/ListOffer.java
+++ app/src/main/java/com/example/katundu/ui/logged/ListOffer.java
@@ -3,12 +3,14 @@ package com.example.katundu.ui.logged;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.res.ColorStateList;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TableRow;
@@ -25,9 +27,14 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.Volley;
 import com.example.katundu.R;
+import com.example.katundu.ui.ControladoraAddProduct;
 import com.example.katundu.ui.ControladoraPresentacio;
 import com.example.katundu.ui.Offer;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -37,6 +44,9 @@ import java.util.ArrayList;
 
 
 public class ListOffer extends AppCompatActivity {
+
+    FirebaseStorage storage = FirebaseStorage.getInstance();
+    StorageReference storageRef = storage.getReference();
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -75,6 +85,7 @@ public class ListOffer extends AppCompatActivity {
         final TextView NomUsuari = findViewById(R.id.nomUsuari);
         final ImageView ImgSettings = findViewById(R.id.img_settings);
         final ImageView Atras = findViewById(R.id.User_Atras);
+        final FloatingActionButton addProduct = findViewById(R.id.FAB_addProduct_LO);
         //USERNAME DEL USUARIO
         NomUsuari.setText(ControladoraPresentacio.getUsername());
 
@@ -130,6 +141,17 @@ public class ListOffer extends AppCompatActivity {
                 //finish();
             }
         });
+
+        addProduct.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ControladoraAddProduct.reset();
+                Intent intent = new Intent(ListOffer.this, AddProduct.class);
+                startActivity(intent);
+                //finish();
+            }
+        });
+
        RequestGetOffers();
     }
 
@@ -189,42 +211,166 @@ public class ListOffer extends AppCompatActivity {
         //Creamos las propiedades de layout que tendrán los botones.
         //Son LinearLayout.LayoutParams porque los botones van a estar en un LinearLayout.
         LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT );
-
+        LinearLayout pareja = new LinearLayout(ListOffer.this);
         //Creamos los botones en bucle
-        for (int i=0; i<numBotones; i++){
-            Button button = new Button(this);
-            //Asignamos propiedades de layout al boton
-            button.setLayoutParams(lp);
-            //Asignamos Texto al botón
-            button.setText(offer_list.get(i).getName() + " - " +offer_list.get(i).getValue() + '€' );
+        //Antes debemos saber si hay un numero par o impar
+        boolean mostrar_producto = true;
+        boolean modo_impar = false;
+        if (numBotones % 2 == 1) modo_impar = true;
+
+        for (int i=0; i<numBotones; ++i){
+            //Modo Layout con pareja, layout de layout con foto+precio+nombre
+            //LinearLayout pareja;
+            if (mostrar_producto && i%2==0) {
+                pareja = new LinearLayout(ListOffer.this);
+                pareja.setOrientation(LinearLayout.HORIZONTAL);
+                TableRow.LayoutParams paramsP = new TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT, TableRow.LayoutParams.WRAP_CONTENT);
+                if (i==0) paramsP.setMargins(0, 20, 0, 20);
+                else paramsP.setMargins(0, 0, 0, 20);
+                pareja.setLayoutParams(paramsP);
+                //pareja.setBackgroundResource(R.drawable.logout_rounded);
+            }
+            //Definimos el layout y lo que contiene (foto+precio+nombre)
+            LinearLayout ll = new LinearLayout(ListOffer.this);
+            ll.setOrientation(LinearLayout.VERTICAL);
+            final ImageView foto = new ImageView(ListOffer.this);
+            TextView preu_producte = new TextView(ListOffer.this);
+            TextView nom_producte = new TextView(ListOffer.this);
+            //Asignamos Texto a los textViews
+            preu_producte.setText(offer_list.get(i).getValue() + "€");
+            nom_producte.setText(offer_list.get(i).getName() + "");
             //Le damos el estilo que queremos
-            button.setBackgroundResource(R.drawable.button_rounded);
-            button.setHint(offer_list.get(i).getName());
-            button.setTextColor(this.getResources().getColor(R.color.colorLetraKatundu));
-            button.setContentDescription(offer_list.get(i).getId());
-            //Margenes del button
-            TableRow.LayoutParams params = new TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT, TableRow.LayoutParams.WRAP_CONTENT);
-            //params.setMargins(left, top, right, bottom);
-            if(i == 0) params.setMargins(0, 20, 0, 20);
-            else params.setMargins(0, 0, 0, 20);
-            button.setLayoutParams(params);
-            //Asignamose el Listener
-            button.setOnClickListener(new ButtonsOnClickListener(this));
-            //Añadimos el botón a la botonera
-            llBotonera.addView(button);
+            //pareja.setBackgroundResource(R.drawable.button_rounded);
+            ll.setBackgroundResource(R.drawable.button_rounded);
+            //foto.setImageURI();
+            StorageReference Reference = storageRef.child("/products/" + offer_list.get(i).getId()).child("product_0");
+            Reference.getBytes(1000000000).addOnSuccessListener(new OnSuccessListener<byte[]>() {
+                @Override
+                public void onSuccess(byte[] bytes) {
+                    Bitmap bmp = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+                    foto.setImageBitmap(bmp);
+                    foto.setVisibility(View.VISIBLE);
+                }
+            });
+            preu_producte.setTextColor(ListOffer.this.getResources().getColor(R.color.colorLetraKatundu));
+            nom_producte.setTextColor(ListOffer.this.getResources().getColor(R.color.colorLetraKatundu));
+            Typeface boldTypeface = Typeface.defaultFromStyle(Typeface.BOLD);
+            preu_producte.setTypeface(boldTypeface);
+            nom_producte.setTypeface(boldTypeface);
+            preu_producte.setTextSize(18);
+            nom_producte.setTextSize(18);
+            //Margenes del layout
+            TableRow.LayoutParams paramsll = new TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT, TableRow.LayoutParams.WRAP_CONTENT);
+            paramsll.weight = 1;
+            paramsll.height = 800;
+            //paramsll.setMargins(left, top, right, bottom);
+            if (i%2==0) paramsll.setMargins(0, 0, 10, 0);
+            else paramsll.setMargins(10, 0, 0, 0);
+            ll.setLayoutParams(paramsll);
+            //Margenes de los textViews
+            TableRow.LayoutParams paramsFoto = new TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT, TableRow.LayoutParams.WRAP_CONTENT);
+            paramsFoto.weight = 1;
+            paramsFoto.setMargins(25, 25, 25, 10);
+            foto.setLayoutParams(paramsFoto);
+            TableRow.LayoutParams paramsPrecio = new TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT, TableRow.LayoutParams.WRAP_CONTENT);
+            paramsPrecio.setMargins(25, 10, 25, 10);
+            preu_producte.setLayoutParams(paramsPrecio);
+            TableRow.LayoutParams paramsN = new TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT, TableRow.LayoutParams.WRAP_CONTENT);
+            //paramsN.weight = 1;
+            paramsN.setMargins(25, 10, 25, 20);
+            nom_producte.setLayoutParams(paramsN);
+            //Le escondemos el nombre del producto en la descripcion
+            ll.setContentDescription(offer_list.get(i).getName());
+            //Asignamose el Listener al Layout dinamico
+            ll.setOnClickListener(new LayoutOnClickListener(ListOffer.this));
+            //Añadimos el layout dinamico al layout
+            ll.addView(foto);
+            ll.addView(preu_producte);
+            ll.addView(nom_producte);
+            if (!mostrar_producto) ll.setVisibility(View.INVISIBLE);
+            pareja.addView(ll);
+            if (mostrar_producto && i%2 == 0) llBotonera.addView(pareja);
+
+            if (modo_impar == true && i==numBotones-1) {
+                --i;
+                mostrar_producto = false;
+                modo_impar = false;
+            }
+            /*
+            //TODO: Se puede borrar pero hasta que no me asegure de que el modo_impar va 100% bien lo dejo por aqui
+            if (modo_impar == true && i==numBotones-1) {
+                //Definimos el layout y lo que contiene (foto+precio+nombre)
+                LinearLayout ll_aux = new LinearLayout(ListOffer.this);
+                ll_aux.setOrientation(LinearLayout.VERTICAL);
+                final ImageView foto_aux = new ImageView(ListOffer.this);
+                TextView preu_producte_aux = new TextView(ListOffer.this);
+                TextView nom_producte_aux = new TextView(ListOffer.this);
+                //Asignamos Texto a los textViews
+                preu_producte_aux.setText(offer_list.get(i).getValue() + "€");
+                nom_producte_aux.setText(offer_list.get(i).getName() + "");
+                //Le damos el estilo que queremos
+                //pareja.setBackgroundResource(R.drawable.button_rounded);
+                ll_aux.setBackgroundResource(R.drawable.button_rounded);
+                //foto.setImageURI();
+                StorageReference Reference2 = storageRef.child("/products/" + ControladoraPresentacio.getOffer_id()).child("product_" + i);
+                Reference2.getBytes(1000000000).addOnSuccessListener(new OnSuccessListener<byte[]>() {
+                    @Override
+                    public void onSuccess(byte[] bytes) {
+                        Bitmap bmp = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+                        foto_aux.setImageBitmap(bmp);
+                        foto_aux.setVisibility(View.VISIBLE);
+                    }
+                });
+                preu_producte_aux.setTextColor(ListOffer.this.getResources().getColor(R.color.colorLetraKatundu));
+                nom_producte_aux.setTextColor(ListOffer.this.getResources().getColor(R.color.colorLetraKatundu));
+                Typeface boldTypeface_aux = Typeface.defaultFromStyle(Typeface.BOLD);
+                preu_producte_aux.setTypeface(boldTypeface_aux);
+                nom_producte_aux.setTypeface(boldTypeface_aux);
+                preu_producte_aux.setTextSize(18);
+                nom_producte_aux.setTextSize(18);
+                //Margenes del layout
+                TableRow.LayoutParams paramsll_aux = new TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT, TableRow.LayoutParams.WRAP_CONTENT);
+                paramsll_aux.weight = 1;
+                paramsll_aux.height = 800;
+                //paramsll.setMargins(left, top, right, bottom);
+                if (i%2==0) paramsll_aux.setMargins(0, 0, 10, 0);
+                else paramsll_aux.setMargins(10, 0, 0, 0);
+                ll_aux.setLayoutParams(paramsll_aux);
+                //Margenes de los textViews
+                TableRow.LayoutParams paramsFoto_aux = new TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT, TableRow.LayoutParams.WRAP_CONTENT);
+                paramsFoto_aux.weight = 1;
+                paramsFoto_aux.setMargins(25, 25, 25, 10);
+                foto_aux.setLayoutParams(paramsFoto_aux);
+                TableRow.LayoutParams paramsPrecio_aux = new TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT, TableRow.LayoutParams.WRAP_CONTENT);
+                paramsPrecio_aux.setMargins(25, 10, 25, 10);
+                preu_producte_aux.setLayoutParams(paramsPrecio_aux);
+                TableRow.LayoutParams paramsN_aux = new TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT, TableRow.LayoutParams.WRAP_CONTENT);
+                //paramsN.weight = 1;
+                paramsN_aux.setMargins(25, 10, 25, 20);
+                nom_producte_aux.setLayoutParams(paramsN_aux);
+                //Asignamose el Listener al Layout dinamico
+                ll_aux.setOnClickListener(new LayoutOnClickListener(ListOffer.this));
+                //Añadimos el layout dinamico al layout
+                ll_aux.addView(foto_aux);
+                ll_aux.addView(preu_producte_aux);
+                ll_aux.addView(nom_producte_aux);
+                ll_aux.setVisibility(View.INVISIBLE);
+                pareja.addView(ll_aux);
+                //if (i%2 == 0) llBotonera.addView(pareja);
+            }*/
         }
     }
-    private class ButtonsOnClickListener implements View.OnClickListener {
-        public ButtonsOnClickListener(ListOffer listOffer) {
-        }
 
+    private class LayoutOnClickListener implements View.OnClickListener {
+        public LayoutOnClickListener(ListOffer listOffer) {
+        }
         @Override
         public void onClick(View view) {
-            Button b = (Button) view;
-            //Provando que funciona el boton
-            Toast.makeText(getApplicationContext(),b.getText(),Toast.LENGTH_SHORT).show();
+            //Provando que funciona el layout en modo boton
+            Toast.makeText(getApplicationContext(),view.getContentDescription().toString(),Toast.LENGTH_SHORT).show();
 
-            Offer info_offer = ControladoraPresentacio.getOffer_perName(b.getHint().toString());
+            //Pasamos los datos del deseo a la controladora
+            Offer info_offer = ControladoraPresentacio.getOffer_perName(view.getContentDescription().toString());
             //Pasamos los datos del deseo a la controladora
             ControladoraPresentacio.setOffer_id(info_offer.getId());
             ControladoraPresentacio.setOffer_name(info_offer.getName());
@@ -236,7 +382,7 @@ public class ListOffer extends AppCompatActivity {
             ControladoraPresentacio.setOffer_PC(info_offer.getKeywords());
             ControladoraPresentacio.setOffer_Value(info_offer.getValue());
             ControladoraPresentacio.setOffer_Description(info_offer.getDescription());
-            //Nos vamos a la ventana de User
+            //Nos vamos a la ventana de EditOffer
             Intent intent = new Intent(ListOffer.this, EditOffer.class);
             startActivity(intent);
             //finish();
--- app/src/main/java/com/example/katundu/ui/logged/VisualizeListOUser.java
+++ app/src/main/java/com/example/katundu/ui/logged/VisualizeListOUser.java
@@ -2,12 +2,14 @@ package com.example.katundu.ui.logged;
 
 import android.content.Intent;
 import android.content.res.ColorStateList;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TableRow;
@@ -28,7 +30,10 @@ import com.example.katundu.R;
 import com.example.katundu.ui.ControladoraPresentacio;
 import com.example.katundu.ui.ControladoraSearchUsers;
 import com.example.katundu.ui.Offer;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -37,6 +42,9 @@ import org.json.JSONObject;
 import java.util.ArrayList;
 
 public class VisualizeListOUser extends AppCompatActivity {
+
+    FirebaseStorage storage = FirebaseStorage.getInstance();
+    StorageReference storageRef = storage.getReference();
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -178,32 +186,94 @@ public class VisualizeListOUser extends AppCompatActivity {
         //Creamos las propiedades de layout que tendrán los botones.
         //Son LinearLayout.LayoutParams porque los botones van a estar en un LinearLayout.
         LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT );
-
+        LinearLayout pareja = new LinearLayout(VisualizeListOUser.this);
         //Creamos los botones en bucle
-        for (int i=0; i<numBotones; i++){
-            Button button = new Button(this);
-            //Asignamos propiedades de layout al boton
-            button.setLayoutParams(lp);
-            //Asignamos Texto al botón
-            button.setText(offer_list.get(i).getName() + " - " +offer_list.get(i).getValue() + '€' );
+        //Antes debemos saber si hay un numero par o impar
+        boolean mostrar_producto = true;
+        boolean modo_impar = false;
+        if (numBotones % 2 == 1) modo_impar = true;
+
+        for (int i=0; i<numBotones; ++i){
+            //Modo Layout con pareja, layout de layout con foto+precio+nombre
+            //LinearLayout pareja;
+            if (mostrar_producto && i%2==0) {
+                pareja = new LinearLayout(VisualizeListOUser.this);
+                pareja.setOrientation(LinearLayout.HORIZONTAL);
+                TableRow.LayoutParams paramsP = new TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT, TableRow.LayoutParams.WRAP_CONTENT);
+                if (i==0) paramsP.setMargins(0, 20, 0, 20);
+                else paramsP.setMargins(0, 0, 0, 20);
+                pareja.setLayoutParams(paramsP);
+                //pareja.setBackgroundResource(R.drawable.logout_rounded);
+            }
+            //Definimos el layout y lo que contiene (foto+precio+nombre)
+            LinearLayout ll = new LinearLayout(VisualizeListOUser.this);
+            ll.setOrientation(LinearLayout.VERTICAL);
+            final ImageView foto = new ImageView(VisualizeListOUser.this);
+            TextView preu_producte = new TextView(VisualizeListOUser.this);
+            TextView nom_producte = new TextView(VisualizeListOUser.this);
+            //Asignamos Texto a los textViews
+            preu_producte.setText(offer_list.get(i).getValue() + "€");
+            nom_producte.setText(offer_list.get(i).getName() + "");
             //Le damos el estilo que queremos
-            button.setBackgroundResource(R.drawable.button_rounded);
-            button.setHint(offer_list.get(i).getName());
-            button.setTextColor(this.getResources().getColor(R.color.colorLetraKatundu));
-            button.setContentDescription(offer_list.get(i).getId());
-            //Margenes del button
-            TableRow.LayoutParams params = new TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT, TableRow.LayoutParams.WRAP_CONTENT);
-            //params.setMargins(left, top, right, bottom);
-            if(i == 0) params.setMargins(0, 20, 0, 20);
-            else params.setMargins(0, 0, 0, 20);
-            button.setLayoutParams(params);
-            //Asignamose el Listener
-            button.setOnClickListener(new VisualizeListOUser.ButtonsOnClickListener());
-            //Añadimos el botón a la botonera
-            llBotonera.addView(button);
+            //pareja.setBackgroundResource(R.drawable.button_rounded);
+            ll.setBackgroundResource(R.drawable.button_rounded);
+            //foto.setImageURI();
+            StorageReference Reference = storageRef.child("/products/" + offer_list.get(i).getId()).child("product_0");
+            Reference.getBytes(1000000000).addOnSuccessListener(new OnSuccessListener<byte[]>() {
+                @Override
+                public void onSuccess(byte[] bytes) {
+                    Bitmap bmp = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+                    foto.setImageBitmap(bmp);
+                    foto.setVisibility(View.VISIBLE);
+                }
+            });
+            preu_producte.setTextColor(VisualizeListOUser.this.getResources().getColor(R.color.colorLetraKatundu));
+            nom_producte.setTextColor(VisualizeListOUser.this.getResources().getColor(R.color.colorLetraKatundu));
+            Typeface boldTypeface = Typeface.defaultFromStyle(Typeface.BOLD);
+            preu_producte.setTypeface(boldTypeface);
+            nom_producte.setTypeface(boldTypeface);
+            preu_producte.setTextSize(18);
+            nom_producte.setTextSize(18);
+            //Margenes del layout
+            TableRow.LayoutParams paramsll = new TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT, TableRow.LayoutParams.WRAP_CONTENT);
+            paramsll.weight = 1;
+            paramsll.height = 800;
+            //paramsll.setMargins(left, top, right, bottom);
+            if (i%2==0) paramsll.setMargins(0, 0, 10, 0);
+            else paramsll.setMargins(10, 0, 0, 0);
+            ll.setLayoutParams(paramsll);
+            //Margenes de los textViews
+            TableRow.LayoutParams paramsFoto = new TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT, TableRow.LayoutParams.WRAP_CONTENT);
+            paramsFoto.weight = 1;
+            paramsFoto.setMargins(25, 25, 25, 10);
+            foto.setLayoutParams(paramsFoto);
+            TableRow.LayoutParams paramsPrecio = new TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT, TableRow.LayoutParams.WRAP_CONTENT);
+            paramsPrecio.setMargins(25, 10, 25, 10);
+            preu_producte.setLayoutParams(paramsPrecio);
+            TableRow.LayoutParams paramsN = new TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT, TableRow.LayoutParams.WRAP_CONTENT);
+            //paramsN.weight = 1;
+            paramsN.setMargins(25, 10, 25, 20);
+            nom_producte.setLayoutParams(paramsN);
+            //Le escondemos el nombre del producto en la descripcion
+            ll.setContentDescription(offer_list.get(i).getName());
+            //Asignamose el Listener al Layout dinamico
+            ll.setOnClickListener(new VisualizeListOUser.LayoutOnClickListener(VisualizeListOUser.this));
+            //Añadimos el layout dinamico al layout
+            ll.addView(foto);
+            ll.addView(preu_producte);
+            ll.addView(nom_producte);
+            if (!mostrar_producto) ll.setVisibility(View.INVISIBLE);
+            pareja.addView(ll);
+            if (mostrar_producto && i%2 == 0) llBotonera.addView(pareja);
+
+            if (modo_impar == true && i==numBotones-1) {
+                --i;
+                mostrar_producto = false;
+                modo_impar = false;
+            }
         }
     }
-
+/*
     private class ButtonsOnClickListener implements View.OnClickListener {
         public ButtonsOnClickListener() {
         }
@@ -229,6 +299,34 @@ public class VisualizeListOUser extends AppCompatActivity {
             ControladoraPresentacio.setOffer_Description(info_offer.getDescription());
             //Nos vamos a la ventana de User
             Intent intent = new Intent(VisualizeListOUser.this, VisualizeOffer.class); //TODO: portar a una nova activity per visualitzar la oferta
+            startActivity(intent);
+            //finish();
+        }
+    }
+*/
+    private class LayoutOnClickListener implements View.OnClickListener {
+        public LayoutOnClickListener(VisualizeListOUser visualizeListOUser) {
+        }
+        @Override
+        public void onClick(View view) {
+            //Provando que funciona el layout en modo boton
+            Toast.makeText(getApplicationContext(),view.getContentDescription().toString(),Toast.LENGTH_SHORT).show();
+
+            //Pasamos los datos del deseo a la controladora
+            Offer info_offer = ControladoraPresentacio.getOffer_perName(view.getContentDescription().toString());
+            //Pasamos los datos del deseo a la controladora
+            ControladoraPresentacio.setOffer_id(info_offer.getId());
+            ControladoraPresentacio.setOffer_name(info_offer.getName());
+            ControladoraPresentacio.setOffer_Categoria(info_offer.getCategory());
+            boolean type = true;
+            String tipus = info_offer.getType();
+            if(tipus.equals("Product")) type = false;
+            ControladoraPresentacio.setOffer_Service(type);
+            ControladoraPresentacio.setOffer_PC(info_offer.getKeywords());
+            ControladoraPresentacio.setOffer_Value(info_offer.getValue());
+            ControladoraPresentacio.setOffer_Description(info_offer.getDescription());
+            //Nos vamos a la ventana de EditOffer
+            Intent intent = new Intent(VisualizeListOUser.this, VisualizeOffer.class);
             startActivity(intent);
             //finish();
         }
