50b69af6a0312e31b065a2b4ac47e7d9ce12b859
annallanza <annallanza@hotmail.com>
2020-04-23 11:48:58 +0200

    Refactor EditarPerfil

--- app/src/main/java/com/example/katundu/ui/logged/EditarPerfil.java
+++ app/src/main/java/com/example/katundu/ui/logged/EditarPerfil.java
@@ -79,7 +79,7 @@ public class EditarPerfil extends AppCompatActivity {
                 "n=" + nameEditText.getText() + "&" +
                 "lat=" + latitudeEditText.getText() + "&" +
                 "lon=" + longitudeEditText.getText() + "&" +
-                "dis=" + maximumDistanceEditText.getText();
+                "distanciamaxima=" + maximumDistanceEditText.getText();
 
         // Request a string response from the provided URL.
         StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
