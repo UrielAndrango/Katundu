ffb73e5441ce1b5e51763038bb45e2f9c7be8079
UrielAndrango <urielandrango@gmail.com>
2020-05-13 10:46:57 +0200

    Ahora puedes acceder a fotos de la Galeria (modifyOffer) + retoque del string del titulo de ModifyWish + retoque del tamaño del ViewText de VisualizeOffer i VisualizeWish de otro usuario

--- app/src/main/java/com/example/katundu/ui/logged/ListTrophies.java
+++ app/src/main/java/com/example/katundu/ui/logged/ListTrophies.java
@@ -19,7 +19,7 @@ public class ListTrophies extends AppCompatActivity {
     //TODO: Sumar 4h (de 14:30 a 18:30) TROPHIES
     //TODO: Sumar 2h (de 20:30 a 22:30) Swipe Refresh
     //TODO: SUmar 0,5h (de 22:30 a 23:00) Trophies en MP, LW, LO, LF
-    //TODO: Sumar 1,5h (de 23:00 a 24:00) Fotos desde la Galeria en AddP y Modify
+    //TODO: Sumar 1,5h (de 23:00 a 24:00) Fotos desde la Galeria en AddP y Modify (no va bien)
     int numero_trofeos = 16;
     ImageView[] lista_trofeos = new ImageView[numero_trofeos];
     Drawable[] vector_icons = new Drawable[numero_trofeos];
