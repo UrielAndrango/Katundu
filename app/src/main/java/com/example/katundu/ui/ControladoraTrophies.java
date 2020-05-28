75a7a078cdaf12497074835cbec8cc46dda92998
UrielAndrango <urielandrango@gmail.com>
2020-05-28 14:13:48 +0200

    Controladora de Trofeos

---
+++ app/src/main/java/com/example/katundu/ui/ControladoraTrophies.java
@@ -0,0 +1,24 @@
+package com.example.katundu.ui;
+
+public class ControladoraTrophies {
+    //private static Boolean[] trofeos_usuario = new Boolean[]{true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true};
+    private static Boolean[] trofeos_usuario = new Boolean[]{false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false};
+
+    public static Boolean[] getTrofeos_usuario() {
+        return trofeos_usuario;
+    }
+
+    public static void setTrofeos_usuario(Boolean[] trofeos_usuario) {
+        ControladoraTrophies.trofeos_usuario = trofeos_usuario;
+    }
+
+    public static Boolean TrofeoConseguido(int pos) {
+        //pos empieza en 0
+        return trofeos_usuario[pos];
+    }
+
+    public static void DesbloquearTrofeo(int pos) {
+        //pos empieza en 0
+        trofeos_usuario[pos] = true;
+    }
+}
--- app/src/main/java/com/example/katundu/ui/logged/ListTrophies.java
+++ app/src/main/java/com/example/katundu/ui/logged/ListTrophies.java
@@ -13,17 +13,15 @@ import androidx.core.graphics.drawable.RoundedBitmapDrawable;
 import androidx.core.graphics.drawable.RoundedBitmapDrawableFactory;
 
 import com.example.katundu.R;
+import com.example.katundu.ui.ControladoraTrophies;
 
 public class ListTrophies extends AppCompatActivity {
 
-    //TODO: Sumar 4h (de 14:30 a 18:30) TROPHIES
-    //TODO: Sumar 2h (de 20:30 a 22:30) Swipe Refresh (OK)
-    //TODO: SUmar 0,5h (de 22:30 a 23:00) Trophies en MP, LW, LO, LF
-    //TODO: Sumar 1,5h (de 23:00 a 24:00) Fotos desde la Galeria en AddP y Modify (OK)
     int numero_trofeos = 16;
     ImageView[] lista_trofeos = new ImageView[numero_trofeos];
     Drawable[] vector_icons = new Drawable[numero_trofeos];
-    Boolean[] trofeos_usuario = new Boolean[]{true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true};
+    //Boolean[] trofeos_usuario = new Boolean[]{true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true};
+    Boolean[] trofeos_usuario = ControladoraTrophies.getTrofeos_usuario();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
