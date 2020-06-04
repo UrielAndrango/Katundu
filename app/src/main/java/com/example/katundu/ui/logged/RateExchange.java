7e703a198624c4b746729ca2583b68613c613411
laiaondono <laia.ondono@gmail.com>
2020-06-04 19:05:17 +0200

    Update RateExchange.java

--- app/src/main/java/com/example/katundu/ui/logged/RateExchange.java
+++ app/src/main/java/com/example/katundu/ui/logged/RateExchange.java
@@ -25,7 +25,7 @@ public class RateExchange extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_rate_exchange);
 
 
